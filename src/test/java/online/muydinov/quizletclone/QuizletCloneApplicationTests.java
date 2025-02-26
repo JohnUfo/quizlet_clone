bb3b308e3d357e9dcc4c771808308703b4a536b6
JohnUfo <tursunov.umidjon.uz@gmail.com>
2025-02-26 05:35:28 +0500

    some change

--- src/test/java/online/muydinov/quizletclone/QuizletCloneApplicationTests.java
+++ src/test/java/online/muydinov/quizletclone/QuizletCloneApplicationTests.java
@@ -2,12 +2,13 @@ package online.muydinov.quizletclone;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class QuizletCloneApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
